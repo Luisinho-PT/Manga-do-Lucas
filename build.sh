98c7253ece81b93681b51d0d9f6f2543eea4f13e
Luis Miguel <luismiguelsous48@gmail.com>
2025-08-26 08:59:02 -0300

    [no-changelog] Ajustando configurações finais da Vercel

--- build.sh
+++ build.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
-
-# 'e' sai se um comando falhar.
-# 'x' mostra os comandos no log.
 set -ex
-
-echo "--- INICIANDO SCRIPT DE BUILD (VERSÃO CORRIGIDA) ---"
-
-# A Vercel já instalou as dependências do requirements.txt.
-# Então, pulamos diretamente para os comandos do Django.
-
-# Usamos 'python3' para garantir que estamos usando a versão correta do Python.
-echo "Rodando migrações..."
+echo "--- INICIANDO SCRIPT DE BUILD ---"
 python3 manage.py migrate
-
-echo "Rodando collectstatic..."
 python3 manage.py collectstatic --noinput --clear
-
-echo "--- SCRIPT DE BUILD CONCLUÍDO COM SUCESSO ---"
+echo "--- SCRIPT DE BUILD CONCLUÍDO ---"
