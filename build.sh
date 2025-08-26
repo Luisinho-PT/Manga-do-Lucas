f094e30118db275bcf7bdf71aece214037a37b9c
Luis Miguel <luismiguelsous48@gmail.com>
2025-08-26 09:39:37 -0300

    [no-changelog] Ajustando configurações finais da Vercel

--- build.sh
+++ build.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-#!/bin/bash
-set -ex  # -e: para o script se der erro | -x: mostra cada comando executado
+set -ex
 
-echo "--- INICIANDO SCRIPT DE BUILD ---"
+# Instala dependências
+python3 -m pip install --upgrade pip
+pip install -r requirements.txt
 
-# aplica migrations (se você realmente precisa no build, senão pode tirar e deixar só no runtime)
-python3 manage.py migrate --noinput
-
-# coleta estáticos
+# Coleta estáticos
 python3 manage.py collectstatic --noinput --clear --verbosity 2
-
-# lista alguns arquivos coletados para aparecer nos logs da Vercel
-ls -la staticfiles | head -20 || true
-
-echo "--- SCRIPT DE BUILD CONCLUÍDO ---"
