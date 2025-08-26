799762a68b80f229b0312cb39d7c32f0578681e8
Luis Miguel <luismiguelsous48@gmail.com>
2025-08-26 09:34:50 -0300

    [no-changelog] Ajustando configurações finais da Vercel

--- manga/wsgi.py
+++ manga/wsgi.py
@@ -2,17 +2,19 @@
 WSGI config for manga project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'manga.settings')
 
-application = get_wsgi_application()
+# Força o collectstatic durante o deploy / inicialização
+try:
+    call_command("collectstatic", interactive=False, clear=True, verbosity=1)
+except Exception as e:
+    print("⚠️ Collectstatic falhou:", e)
 
+application = get_wsgi_application()
 app = application
